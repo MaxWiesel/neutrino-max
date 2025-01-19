0b0d834e372c0e83ee28579dd86fb48dfb8011d0
vanhofen <vanhofen@gmx.de>
2025-01-19 21:42:16 +0100

    - timerlist: mark remote timers with different text colors

--- src/gui/timerlist.cpp
+++ src/gui/timerlist.cpp
@@ -1232,6 +1232,15 @@ void CTimerList::paintItem(int pos)
 	if (currpos < timerlist.size())
 	{
 		CTimerd::responseGetTimer &timer = timerlist[currpos];
+		if (timer.eventType == CTimerd::TIMER_REMOTEBOX)
+		{
+			// mark remote timers with different text colors
+			if (i_selected || i_marked)
+				color = COL_MENUCONTENTSELECTED_TEXT_PLUS_2;
+			else
+				color = COL_MENUCONTENTINACTIVE_TEXT;
+
+		}
 		char zAlarmTime[25] = {0};
 		struct tm *alarmTime = localtime(&(timer.alarmTime));
 		strftime(zAlarmTime, 20, "%d.%m. %H:%M", alarmTime);
